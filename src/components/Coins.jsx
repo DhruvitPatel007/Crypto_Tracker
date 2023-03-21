3c55c84dfc66176c109f99066b32ab418a580ef6
Dhruvit <pateldhruvit0712@gmail.com>
2023-03-21 12:33:21 +0530

    Half completed

---
+++ src/components/Coins.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import { useEffect } from "react";
+import { Container, Heading, VStack, Text, Image } from "@chakra-ui/react";
+import Loader from "./Loader";
+import { HStack } from "@chakra-ui/react";
+import ErrorComponent from "./ErrorComponent";
+
+const Coins = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [currency, setCurrency] = useState("inr");
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/markets?vs_currency=${currency}&page=${page}`);
+        console.log(data);
+        setCoins(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, [currency,page]);
+
+  if (error) return <ErrorComponent message={"Error while fetching coins"} />;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"}>
+            {coins.map((i) => (
+              <CoinsCard
+                key={i.id}
+                name={i.name}
+                img={i.image}
+                rank={i.trust_score_rank}
+                url={i.url}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+const CoinsCard = ({ name, img, rank, url }) => (
+  <a href={url} target={"blank"}>
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        w={"10"}
+        h={"10"}
+        objectFit={"contain"}
+        alt={"Exchanges"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
+      <Text noOfLines={"1"}>{name}</Text>
+    </VStack>
+  </a>
+);
+
+export default Coins;

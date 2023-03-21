8aefeabb35beb1b6e85be388c69fe6e2f982a016
Dhruvit <pateldhruvit0712@gmail.com>
2023-03-22 00:52:21 +0530

    third commit

--- src/components/CoinDetails.jsx
+++ src/components/CoinDetails.jsx
@@ -1,9 +1,172 @@
-import React from 'react'
+import {
+  Container,
+  Box,
+  RadioGroup,
+  Radio,
+  HStack,
+  VStack,
+  Text,
+  Image,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  Badge,
+  Progress,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import React from "react";
+import Loader from "./Loader";
+import {useParams} from "react-router-dom";
+import { server } from "..";
+import { useEffect } from "react";
+import ErrorComponent from "./ErrorComponent";
 
 const CoinDetails = () => {
-  return (
-    <div>CoinDetails</div>
-  )
-}
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
+  const params = useParams();
 
-export default CoinDetails
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/${params.id}`);
+        console.log(data);
+        setCoin(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchCoin();
+  }, [params.id]);
+
+  if (error)
+    return <ErrorComponent message={"Error while fetching coin details"} />;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderWidth={1}>
+            Coin Details
+          </Box>
+
+          <RadioGroup value={currency} onChange={setCurrency} p={"8"}>
+            <HStack>
+              <Radio value={"inr"}>INR</Radio>
+              <Radio value={"usd"}>USD</Radio>
+              <Radio value={"eur"}>EURO</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <VStack p={"16"} spacing={"4"} alignItems={"flex-start"}>
+            <Text fontSize={"small"} alignSelf={"center"} opacity={"0.7"}>
+              Last Updated on{" "}
+              {Date(coin.market_data.last_updated).split("G")[0]}
+            </Text>
+            <Image
+              src={coin.image.large}
+              w={"16"}
+              h={"16"}
+              objectFit={"contain"}
+            />
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+              <StatNumber>
+                {currencySymbol}
+                {coin.market_data.current_price[currency]}
+              </StatNumber>
+              <StatHelpText>
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? "increase"
+                      : "decrease"
+                  }
+                ></StatArrow>
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
+            </Stat>
+
+            <Badge fontSize={"2xl"} bgColor={"blackAlpha.900"} color={"white"}>
+              {`#${coin.market_cap_rank}`}
+            </Badge>
+
+            <CustomBar
+              high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+              low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}
+            />
+
+            <Box w={"full"} p={"4"}>
+              <Item
+                title={"Max Supply"}
+                value={
+                  coin.market_data.max_supply
+                    ? coin.market_data.max_supply
+                    : "Info is not available"
+                }
+              />
+              <Item
+                title={"Circulating Supply"}
+                value={
+                  coin.market_data.circulating_supply
+                    ? coin.market_data.circulating_supply
+                    : "Info is not available"
+                }
+              />
+
+              <Item
+                title={"Market Cap"}
+                value={`${currencySymbol}${coin.market_data.market_cap[currency]}`}
+              />
+
+              <Item
+                title={"All Time High"}
+                value={`${currencySymbol}${coin.market_data.ath[currency]}`}
+              />
+
+              <Item
+                title={"All Time Low"}
+                value={`${currencySymbol}${coin.market_data.atl[currency]}`}
+              />
+            </Box>
+          </VStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+const CustomBar = ({ high, low }) => (
+  <VStack w={"full"}>
+    <Progress value={50} colorScheme={"teal"} w={"full"} />
+    <HStack justifyContent={"space-between"} w={"full"}>
+      <Badge children={low} colorScheme={"red"} />
+      <Text fontSize={"sm"}>24H Range</Text>
+      <Badge children={high} colorScheme={"green"} />
+    </HStack>
+  </VStack>
+);
+
+const Item = ({ title, value }) => (
+  <HStack justifyContent={"space-between"} w={"full"} my={"4"}>
+    <Text fontFamily={"Bebas Neue"} letterSpacing={"widest"}>
+      {title}
+    </Text>
+    <Text>{value}</Text>
+  </HStack>
+);
+
+export default CoinDetails;
